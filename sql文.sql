a70ea3c430a4de992d8a7d3dfc83b818bfd6ada9
ryo <68486607+swiftryo@users.noreply.github.com>
2021-01-16 06:36:47 +0900

    first commit

---
+++ sql文.sql
@@ -0,0 +1,52 @@
+-- すべてのユーザー情報
+use mydb;
+select * from users;
+
+-- ユーザー情報のidと名字のみ
+use mydb;
+select id, last_name from users;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
